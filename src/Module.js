17a53e921ebcf260ad8e2f79010214f064105d74
da-matsumoto <da.mats1990@gmail.com>
2018-05-09 22:20:24 +0900

    first commit

---
+++ src/Module.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Message = styled.p`
+    font-size: 28px;
+    color: red;
+`
+const Module = () => {
+    return (
+        <Message>module</Message>
+    )
+}
+
+export default Module
